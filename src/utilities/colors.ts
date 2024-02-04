1fec28ef7f1ba29c57d4b582b3be0cadf60ddc56
alireza <alireza.alemi76@icloud.com>
2024-02-03 20:57:29 -0700

    added color palette.

---
+++ src/utilities/colors.ts
@@ -0,0 +1,5 @@
+export const colors = {
+    primary: {green: "#495E57", yellow: "#F4CE14"},
+    secondary: {lightRed: "#FBDABB", darkRed: "#EE9972"},
+    highlight: {lightGray: "#EDEFEE", darkGray: "#333333"}
+}
